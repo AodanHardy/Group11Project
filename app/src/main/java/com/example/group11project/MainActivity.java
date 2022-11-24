d068a5f2b92973c7c6cf80188a2ccb19d00b7c0d
Aodán Hardy <114144196+AodanHardy@users.noreply.github.com>
2022-11-24 13:01:20 +0000

    Update MainActivity.java

--- app/src/main/java/com/example/group11project/MainActivity.java
+++ app/src/main/java/com/example/group11project/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView txtTest = (TextView) findViewById(R.id.txtTest);
-        txtTest.setText("Testing");
+        
     }
 }
