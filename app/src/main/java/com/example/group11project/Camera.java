95ece20fc217a3ee84d93bc3cb041b3e0f9ed231
paul <Muldoon-P5@ulster.ac.uk>
2022-11-30 14:27:18 +0000

    added functionality to main menu buttons

--- app/src/main/java/com/example/group11project/Camera.java
+++ app/src/main/java/com/example/group11project/Camera.java
@@ -10,5 +10,6 @@ public class Camera extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
--- app/src/main/java/com/example/group11project/MainActivity.java
+++ app/src/main/java/com/example/group11project/MainActivity.java
@@ -2,14 +2,41 @@ package com.example.group11project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    Button openCameraActivity;
+    Button openSubmissionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        openCameraActivity = (Button) findViewById(R.id.btn_newsubmission);
+        openSubmissionList = (Button) findViewById(R.id.btn_mysubmissions);
+
+        openCameraActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Camera.class);
+                startActivity(i);
+            }
+        });
+
+        openSubmissionList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, MySubmissions.class);
+                startActivity(i);
+            }
+        });
     }
 }
