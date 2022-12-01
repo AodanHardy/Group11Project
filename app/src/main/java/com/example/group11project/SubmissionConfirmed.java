c6d1127e5aadfddc8a64b7c88b1b3850cf86c4be
csf21haa <hardy-a4@ulster.ac.uk>
2022-12-01 14:26:28 +0000

    Maps

--- app/src/main/java/com/example/group11project/SubmissionConfirmed.java
+++ app/src/main/java/com/example/group11project/SubmissionConfirmed.java
@@ -1,16 +1,19 @@
 package com.example.group11project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class SubmissionConfirmed extends AppCompatActivity {
-
+public class SubmissionConfirmed extends AppCompatActivity implements OnMapReadyCallback{
+    final String GOOGLE_MAP_API_KEY = "AIzaSyD3FAItQMa4nv-8WVuMjFhWmMAcJjTV2bk";
     double lat;
     double lon;
-    MapView mapView;
+
 
 
 
@@ -18,12 +21,20 @@ public class SubmissionConfirmed extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submission_confirmed);
-
         lat = 57.927827;
         lon = -67.93722;
 
+        SupportMapFragment mapFragment= (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
+        mapFragment.getMapAsync(this);
 
 
+
+
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
 
     }
 }
