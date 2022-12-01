dcc23e973a6168b46756508d844cad36b970a0d2
csf21haa <hardy-a4@ulster.ac.uk>
2022-12-01 12:38:10 +0000

    submission buttons

--- app/src/main/java/com/example/group11project/SubmissionConfirmed.java
+++ app/src/main/java/com/example/group11project/SubmissionConfirmed.java
@@ -6,9 +6,17 @@ import android.os.Bundle;
 
 public class SubmissionConfirmed extends AppCompatActivity {
 
+    double lat;
+    double lon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submission_confirmed);
+
+        lat = 57.927827;
+        lon = -67.93722;
+
+
     }
 }
