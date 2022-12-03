5348c5bd05cc8523691a410c24801a6667e5aade
Aodán Hardy <hardy-a4@ulster.ac.uk>
2022-12-03 14:14:29 +0000

    Maps now showing location

--- app/src/main/java/com/example/group11project/SubmissionConfirmed.java
+++ app/src/main/java/com/example/group11project/SubmissionConfirmed.java
@@ -1,34 +1,127 @@
 package com.example.group11project;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class SubmissionConfirmed extends AppCompatActivity implements OnMapReadyCallback{
     // Aodan
     final String GOOGLE_MAP_API_KEY = "AIzaSyD3FAItQMa4nv-8WVuMjFhWmMAcJjTV2bk";
-    double lat;
-    double lon;
+
+    private GoogleMap mMap;
+
+    final private int REQUEST_COURSE_ACCESS = 123;
+    boolean permissionGranted = false;
+    LocationManager lm;
+    LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submission_confirmed);
-        lat = 57.927827;
-        lon = -67.93722;
+
 
         SupportMapFragment mapFragment= (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
         mapFragment.getMapAsync(this);
 
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case REQUEST_COURSE_ACCESS:
+                if (grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    permissionGranted = true;
+                    if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+                        return;
+                    }
+                    lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,0, locationListener);
+                }else permissionGranted = false;
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if(ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED&&
+                ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{
+                    ACCESS_COARSE_LOCATION
+            },REQUEST_COURSE_ACCESS);
+            return;
+        }else permissionGranted = true;
+        if (permissionGranted){
+            lm.removeUpdates(locationListener);
+        }
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new MyLocationListener();
 
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED&&
+        ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+            ACCESS_FINE_LOCATION}, REQUEST_COURSE_ACCESS);
+        }else permissionGranted = true;
+
+
+        if (permissionGranted)lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,0 , locationListener);
+
+    }
+
+    private class MyLocationListener implements LocationListener{
+
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            if (location != null){
+                LatLng p = new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(p).title("Location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(p, 12f));
+            }
+        }
+
+        @Override
+        public void onProviderEnabled(@NonNull String provider) {
+            LocationListener.super.onProviderEnabled(provider);
+        }
+
+        @Override
+        public void onProviderDisabled(@NonNull String provider) {
+            LocationListener.super.onProviderDisabled(provider);
+        }
     }
 }
